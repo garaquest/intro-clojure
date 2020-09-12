a0813ad7149a6780f70ed36d483f455c06381857
garaquest <garaquest@gmail.com>
2020-09-13 00:42:26 +0700

    Literal

---
+++ literal.clj
@@ -0,0 +1,27 @@
+;Numeric Types
+42 ;integer
+-1.5 ;floating point
+22/7 ;ratio
+
+
+;Character Types
+"hello" ;string
+\e ;character
+#"[0-9]+" ;regular expression
+
+
+;Symbol and indents
+map ; symbol
++ ;symbol - most punctuation allowed
+clojure.core/+ ;namespaced symbol
+nil ; null value
+true false ;booleans
+:alpha ;keyword
+:release/alpha ;keyword with namespace
+
+
+;literal collections
+'(1 2 3) ; list
+[1 2 3] ; vector
+#{1 2 3} ;set
+{:a 1, :b 2} ;map
